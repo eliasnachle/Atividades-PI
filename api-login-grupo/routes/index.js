afbe9f528b9990067da4861c2644e4304b80a85a
eliasnachle <elias.nachle@bandtec.com.br>
2021-05-20 01:36:02 -0300

    API Grupo

---
+++ api-login-grupo/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const app = new express();
+// app.use(express.static('/public'));
+
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   res.sendFile(path.resolve(__dirname, 'public/', 'login.html'));
+
+// });
+
+module.exports = router;
